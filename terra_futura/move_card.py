57b244bb110fa1920ad453befa254e91ee4ff36d
nothingcanbexplained <master20112006@gmail.com>
2025-11-30 12:51:46 +0200

    Fix linting

--- terra_futura/move_card.py
+++ terra_futura/move_card.py
@@ -2,7 +2,7 @@ from terra_futura.interfaces import InterfacePile, InterfaceGrid
 from terra_futura.simple_types import GridPosition
 
 class MoveCard:
-    def move_card(self, pile: InterfacePile, index: int, 
+    def move_card(self, pile: InterfacePile, index: int,
                   grid_coordinate: GridPosition, grid: InterfaceGrid) -> bool:
         if not grid.can_put_card(grid_coordinate):
             return False
@@ -11,10 +11,10 @@ class MoveCard:
             return False
 
         taken_card = pile.take_card(index)
-        
+
         if taken_card is None:
             return False
 
         grid.put_card(grid_coordinate, taken_card)
-        
+
         return True
